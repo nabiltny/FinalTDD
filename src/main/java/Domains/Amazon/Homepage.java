a2eabdbdbe1aae7b70b76980e6fa87fd3316006a
nabiltny <nabiltny01@gmail.com>
2023-12-09 22:52:28 -0500

    amazon base

--- src/main/java/Domains/Amazon/Homepage.java
+++ src/main/java/Domains/Amazon/Homepage.java
@@ -6,23 +6,101 @@ import org.openqa.selenium.support.How;
 import org.testng.Assert;
 
 import static Domains.Amazon.WebElements.*;
-public class Homepage extends WebAPI{
+public class Homepage extends WebAPI {
+    public final String username = "nblauto123@gmail.com";
+    public final String password = "nblPnt23";
+    @FindBy(how = How.XPATH, using = topSearchBar)
+    public WebElement searchBar;
+    @FindBy(how = How.XPATH, using = topSearchButton)
+    public WebElement searchButton;
+    @FindBy(how = How.XPATH, using = signInAnchor)
+    public WebElement signInButton;
+    @FindBy(how = How.XPATH, using = emailField)
+    public WebElement emailIF;
+    @FindBy(how = How.XPATH, using = signInContinue)
+    public WebElement signInSubmit;
+    @FindBy(how = How.XPATH, using = createAccSubmit)
+    public WebElement createAccountSubmit;
+    @FindBy(how = How.XPATH, using = customerNameinput)
+    public WebElement customerNameIF;
+    @FindBy(how = How.XPATH, using = customerEmailinput)
+    public WebElement customerEmailIF;
+    @FindBy(how = How.XPATH, using = customerPasswordInput)
+    public WebElement customerPasswordIF;
+    @FindBy(how = How.XPATH, using = customerPasswordCheckInput)
+    public WebElement customerPasswordCheckIF;
+    @FindBy(how = How.XPATH, using = createAccSubmit)
+    public WebElement createAccountSubmitButton;
 
-    @FindBy (how = How.XPATH, using=topSearchBar) public WebElement searchBar;
-    @FindBy (how = How.XPATH, using=topSearchButton) public WebElement searchButton;
-    public void searchFieldAction(){
+    public void searchFieldAction() {
         searchBar.click();
-
     }
-    public void searchFor(String s){
-        inputValueInTextBoxByWebElement(searchBar,s);
 
+    public void searchFor(String s) {
+        inputValueInTextBoxByWebElement(searchBar, s);
     }
+
+    public void clickSignIn() {
+        signInButton.click();
+    }
+
+    public void emailFieldFill(String s) {
+        emailIF.click();
+        inputValueInTextBoxByWebElement(emailIF, s);
+    }
+
+    public void signInContinue() {
+        signInSubmit.click();
+    }
+
+    public void clickCreateAccount() {
+        createAccountSubmit.click();
+    }
+
+    public void customerNameFill(String s) {
+        customerNameIF.click();
+        inputValueInTextBoxByWebElement(customerNameIF, s);
+    }
+
+    public void customerEmailFill(String s) {
+        customerEmailIF.click();
+        inputValueInTextBoxByWebElement(customerEmailIF, s);
+    }
+
+    public void customerSignUpPasswordFill(String s) {
+        customerPasswordIF.click();
+        inputValueInTextBoxByWebElement(customerPasswordIF, s);
+        customerPasswordCheckIF.click();
+        inputValueInTextBoxByWebElement(customerPasswordCheckIF, s);
+    }
+    public void customerSignInPasswordFill(String s) {
+        customerPasswordIF.click();
+        inputValueInTextBoxByWebElement(customerPasswordIF, s);
+    }
+
+    public void createAccountSubmitButton() {
+        createAccountSubmitButton.click();
+    }
+
 
     public void TestCase1() throws InterruptedException {
         searchFieldAction();
         searchFor("Nikes");
     }
 
+    //This test cases finds and fills out the create account form. This is the farthest I can go
+    //Without running into bot authentication questions from the domain.
+    public void TestCase2() {
+        clickSignIn();
+        clickCreateAccount();
+        customerNameFill("Nabil Testing");
+        customerEmailFill("nblauto123@gmail.com");
+        customerSignUpPasswordFill("nblPnt23");
+    }
+    //Sign in
+    public void TestCase3(){
+        clickSignIn();
+        emailFieldFill(username);
+        customerSignInPasswordFill(password);
+    }
 }
-
--- src/main/java/Domains/Amazon/WebElements.java
+++ src/main/java/Domains/Amazon/WebElements.java
@@ -2,5 +2,15 @@ package Domains.Amazon;
 
 public class WebElements {
     public static final String topSearchBar = "//input[@id='twotabsearchtextbox']";
-    public static final String topSearchButton = "//input[@id=\"nav-search-submit-button\"]";
+    public static final String topSearchButton = "//input[@id='nav-search-submit-button']";
+    public static final String signInAnchor = "//a[@id='nav-link-accountList']";
+    public static final String emailField = "//input[@type='email'']";
+    public static final String signInContinue = "//input[@id='continue']";
+    public static final String createAccSubmit = "//a[@id='createAccountSubmit']";
+    public static final String customerNameinput = "//input[@id='ap_customer_name']";
+    public static final String customerEmailinput = "//input[@id='ap_email']";
+    public static final String customerPasswordInput = "//input[@id='ap_password']";
+    public static final String customerPasswordCheckInput = "//input[@id='ap_password_check']";
+    public static final String createAccountSubmit = "//input[@id='continue']";
+
 }
